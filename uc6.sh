01e2181e077bdc68d2599bcf60d93473585a2a31
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-03 12:37:36 +0530

    firstcommit

--- uc6.sh
+++ uc6.sh
@@ -1,9 +1,12 @@
 #! /bin/bash -x
-declare -A dictionaryVar
+declare -A com
 a=9
 b=3
 c=7
-com=$(((a*b)+c))
+com[1]=$(((a*b)+c))
+com[2]=$(((a+b)*c))
+com[3]=$(((a%b)*c))
+com[4]=$((c+(a/b)))
 echo "output is- " $com
 
 echo "dictionaryVar - " ${com[@]}
