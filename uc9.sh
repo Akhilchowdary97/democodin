00e134bdb8362dd11e1551fbe8071372d4332089
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-03 13:16:34 +0530

    first commit

---
+++ uc9.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -x
+#declare -A com
+#a=9
+#b=3
+#c=7
+#com[1]=$(((a*b)+c))
+#com[2]=$(((a+b)*c))
+#com[3]=$(((a%b)*c))
+#com[4]=$((c+(a/b)))
+com[1]=34
+com[2]=84
+com[3]=0
+com[4]=10
