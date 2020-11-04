43151adb4a8db92fbb2fe5694c5213458144be5c
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-04 19:00:25 +0530

    First Commit

---
+++ ucB1_sampleemail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+read -p "Enter string -  " userInput
+
+email_pattern="^[a-z]{3}\@$";
+
+if [[ $userInput =~ $email_pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
+#correct validation showing in regex101
