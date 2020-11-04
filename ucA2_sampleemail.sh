8043ae07bf3f01ac08b8c92651c309146035de23
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-04 16:13:40 +0530

    First Commit

---
+++ ucA2_sampleemail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+read -p "Enter string -  " userInput
+
+email_pattern="^(abc)\-(100)*\@(yahoo)*\.(com)$";
+
+if [[ $userInput =~ $email_pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
