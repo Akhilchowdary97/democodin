fc24353a03b76d11e56747e7ddaa2909087f48fa
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-11-05 13:07:42 +0530

    First commit

---
+++ ucB11_sampleemail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+read -p "Enter string -  " userInput
+
+email_pattern="^[a-zA-Z]{3,}\@(gmail)\.(com)$";
+
+if [[ $userInput =~ $email_pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
